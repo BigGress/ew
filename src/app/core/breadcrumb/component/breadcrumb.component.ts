befdc16362587df19a923702e88b8f0e88ff8017
Lin <lin@Lins-iMac.local>
2018-01-22 10:18:08 +0800

    feat(breadcrumb): change inline template

--- src/app/core/breadcrumb/component/breadcrumb.component.ts
+++ src/app/core/breadcrumb/component/breadcrumb.component.ts
@@ -15,7 +15,13 @@ export interface Breadcrumb {
 
 @Component({
   selector: 'ew-breadcrumb',
-  templateUrl: './breadcrumb.component.html'
+  template: `
+  <nz-breadcrumb>
+      <nz-breadcrumb-item *ngFor="let item of breadcrumbs">
+          <a [routerLink]="[item?.link, item.params]">{{item.title}}</a>
+      </nz-breadcrumb-item>
+  </nz-breadcrumb>
+  `
 })
 
 export class EwBreadcrumbComponent implements OnInit, OnDestroy {
